c673c252b2d9f1f17e575b01f6ad865c71b80bc4
wirelessphreak <42366453+wirelessphreak@users.noreply.github.com>
2021-02-18 15:04:43 -0800

    Update collect_google_ip_addresses.sh

--- collect_google_ip_addresses.sh
+++ collect_google_ip_addresses.sh
@@ -10,6 +10,7 @@ recaptcha_netblocks="_netblocks.google.com _netblocks3.google.com"
 # Collects the Google Corp IP addresses and their CIDR notation from Google's netblocks
 ip_addresses=`dig -t txt $recaptcha_netblocks +short | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])/[0-9][0-9]?'`
 ip_address_file=./GoogleIPs.txt
+git commit ./GoogleIPs.txt -m 'commit via automated script'
 
 # Write the Google Corp's IP addresses to a file
 echo "$ip_addresses" > $ip_address_file
